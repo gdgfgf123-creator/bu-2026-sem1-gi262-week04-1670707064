b558f8f36620a1e628a40dffb00117b80720f646
gdgfgf123-creator <gdgfgf123@gmail.com>
2026-09-08 10:57:18 +0700

    ทำในห้อง

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -10,7 +10,7 @@ namespace Assignment
             // LCT01_SyntaxList();
             // LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+             LCT04_SyntaxDictionary();
         }
 
         #region Lecture
@@ -22,7 +22,47 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            //string[] playerName = new string[20];
+            LinkedList<string> linkedlist= new LinkedList<string>();
+
+
+            //[Node 1]
+            linkedlist.AddLast("Node 1");
+
+            //[Node 1]<--[Node 2]
+            linkedlist.AddLast("Node 2");
+
+            //[Node 0]<--[Node 1]<--[Node 2]
+            linkedlist.AddFirst("Node 0");
+
+            LinkedListNode<string> firstNode = linkedlist.First;
+            Debug.Log("firs: " + firstNode.Value);
+
+            LinkedListNode<string> LastNode = linkedlist.Last;
+            Debug.Log("Last: " + LastNode.Value);
+
+            Debug.Log("firstNode.Next: " + firstNode.Next.Value);
+            Debug.Log("firstNode.Next.Next: " + firstNode.Next.Next.Value);
+
+            Debug.Log("lastNode.Previons : "+ LastNode.Previous.Value);
+            Debug.Log("lastNode.Previons.Previous : " + LastNode.Previous.Previous.Value);
+
+            if (firstNode.Previous == null) Debug.Log("FirstNOde.Previous == null");
+            if (LastNode.Next == null) Debug.Log("LastNode.Next == null");
+            //[Node 0]<--[Node 0.5]<--[Node 1]<--[Node 2]
+            linkedlist.AddAfter(firstNode, "Node 0.5");
+
+            //[Node 0]<--[Node 0.5]<--[Node 1]<--[Node 1.5]<--[Node 2]
+            linkedlist.AddBefore(LastNode, "Node 1.5");
+
+            LinkedListNode<string> node1 = linkedlist.Find("Node 1");
+
+            linkedlist.Remove("Node 1");
+            linkedlist.Remove(node1);
+            linkedlist.RemoveLast();
+            linkedlist.RemoveFirst();
+
+            linkedlist.Clear();
         }
 
         public void LCT03_SyntaxHashTable()
@@ -32,7 +72,50 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<string,int>inv = new Dictionary<string,int>();
+            var inv2 = new Dictionary<string,int>();
+
+            //"potion 1
+            inv.Add("//potion", 1);
+
+            //potion 1
+            //Apple : 10
+            inv.Add("Apple", 10);
+            //potion 1
+            //Apple : 10
+            //Banana : 5
+            inv["Banana"] = 5;
+            //potion 5
+            //Apple : 10
+            //Banana : 5
+            inv["Pontion"] = 10;
+
+            var pickupItem = "Sword";
+            inv[pickupItem] = 1;
+
+            //foreach(KeyValuePair<string,int>pair in inv)
+            foreach (var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
+
+            var appleExists = inv.ContainsKey("Apple");
+            Debug.Log(appleExists);
+
+            var keyExists = inv.ContainsKey("Key");
+            Debug.Log(keyExists);
+            inv.Remove("Apple");
+
+            foreach(var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
+
+            
         }
 
         #endregion
